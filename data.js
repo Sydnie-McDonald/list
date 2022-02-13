af4a2cef8c2716c7da08a204de7f341e1fa26874
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-13 10:56:10 -0600

    adding dog card on main index, adjusting photos to correctly render and making detail page appear.

---
+++ data.js
@@ -0,0 +1,34 @@
+export const dogs = [
+    {
+        id: '1',
+        name: 'Lucky',
+        breed: 'American-terrier',
+        type: 1,
+        weight: '60lbs',
+        color: 'black',
+    },
+    {
+        id: '2',
+        name: 'Spotty',
+        breed: 'Lab',
+        type: 2,
+        weight: '40lbs',
+        color: 'brown',
+    },
+    {
+        id: '3',
+        name: 'Chauncy',
+        breed: 'Pitbull',
+        type: 3,
+        weight: '72lbs',
+        color: 'grey',
+    },
+    {
+        id: '4',
+        name: 'Alex',
+        breed: 'Great Dane',
+        type: 4,
+        weight: '80lbs',
+        color: 'brown',
+    }
+];
---
+++ render.js
@@ -0,0 +1,56 @@
+export function renderDogCard(dog) {
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+
+    div.classList.add('dog-card');
+
+    p.textContent = dog.name;
+    img.src = `./assets/${dog.type}.jpg`;
+    a.href = `/lists/?id=${dog.id}`; // link to the dog's detail page here
+
+    div.append(p, img);
+
+    a.append(div);
+
+    return a;
+}
+
+export function renderDog(dog) {
+    const div = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const breedEl = document.createElement('p');
+    const weightEl = document.createElement('p');
+    const colorEl = document.createElement('p');
+    const colorAndweightEl = document.createElement('div');
+
+    div.classList.add('dog-detail-container');
+    // create anchor tag
+    const a = document.createElement('a');
+    a.href = `/lists/?id=${dog.id}`;
+
+    const img = document.createElement('img');
+    img.src = `./assets/${dog.type}.jpg`;
+
+    nameEl.textContent = dog.name;
+    nameEl.classList.add('name');
+
+    breedEl.textContent = dog.breed;
+    breedEl.classList.add('breed');
+
+    weightEl.textContent = dog.weight;
+    weightEl.classList.add('weight');
+
+    colorEl.textContent = dog.color;
+    colorEl.classList.add('color');
+
+    colorAndweightEl.classList.add('color-and-weight');
+
+    colorAndweightEl.append(colorEl, weightEl);
+
+
+    a.append(img, nameEl, breedEl, colorAndweightEl);
+    div.append(a);
+    return div;
+}
